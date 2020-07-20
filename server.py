a12559040351d10152c37ab78060c1507323b4dc
117503445 <t117503445@gmail.com>
2020-07-20 12:43:49 +0800

    写后端逻辑

--- server.py
+++ server.py
@@ -1,3 +1,16 @@
-import flask
+from flask import Flask, escape, request
+import file_util
 
-print('Hello World')
+app = Flask("XDU Check In")
+
+dir_data = 'data'
+
+
+def init():
+    file_util.create_dir_if_not_exist(dir_data)
+
+
+if __name__ == "__main__":
+    init()
+
+    app.run(host='0.0.0.0', port='8080')
